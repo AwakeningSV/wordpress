e9abcedc1fb9c6319a3da03423d1426f2e863d74
Reid Burke <me@reidburke.com>
2021-03-05 11:23:42 -0800

    Remove unused tags from HTML head.

--- site/web/app/themes/jenner/header.php
+++ site/web/app/themes/jenner/header.php
@@ -9,22 +9,12 @@
         <title><?php wp_title(''); echo ' – ';  bloginfo( 'name' ); ?></title>
         <?php endif; ?>
 
-		<?php // mobile meta (hooray!) ?>
-		<meta name="HandheldFriendly" content="True">
-		<meta name="MobileOptimized" content="320">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
 		<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
 		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 
-		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-
-		<?php // wordpress head functions ?>
 		<?php wp_head(); ?>
-		<?php // end of wordpress head ?>
-
-		<?php // drop Google Analytics Here ?>
-		<?php // end analytics ?>
 	</head>
 
 	<body <?php body_class(); ?>>
