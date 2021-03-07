5cb4a04d9f83f314a2b610497fc58c6e42325d49
Reid Burke <me@reidburke.com>
2021-03-06 17:53:50 -0800

    RefTagger -> ESV CrossRef

--- site/web/app/themes/jenner/footer.php
+++ site/web/app/themes/jenner/footer.php
@@ -19,6 +19,11 @@
 		</div> <?php // end #container ?>
 
 		<?php // all js scripts are loaded in library/bones.php ?>
+        <script>
+            window.ESV_CROSSREF_OPTIONS = {
+                header_font_color: '000000'
+            };
+        </script>
 		<?php wp_footer(); ?>
 
 	</body>
