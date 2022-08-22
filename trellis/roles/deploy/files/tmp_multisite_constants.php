9d75e11a1f14ed8d4b7507d8b151c405a359f00f
Reid Burke <reid@reidburke.com>
2022-08-21 17:13:18 -0700

    Remove plugin vars from tmp_multisite_constants.
    See: https://github.com/roots/trellis/issues/1385

--- trellis/roles/deploy/files/tmp_multisite_constants.php
+++ trellis/roles/deploy/files/tmp_multisite_constants.php
@@ -2,6 +2,7 @@
 error_reporting(E_ALL & ~E_NOTICE);
 define('MULTISITE', false);
 define('SUBDOMAIN_INSTALL', false);
-define('WPMU_PLUGIN_DIR', '');
-define('WP_PLUGIN_DIR', '');
+// https://github.com/roots/trellis/issues/1385
+// define('WPMU_PLUGIN_DIR', '');
+// define('WP_PLUGIN_DIR', '');
 define('WP_USE_THEMES', false);
