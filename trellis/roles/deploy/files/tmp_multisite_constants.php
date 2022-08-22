e1d208647848a9b7ce66109de7ed23cb25e9ef53
Reid Burke <reid@reidburke.com>
2022-08-21 17:08:35 -0700

    Update tmp_multisite_constants for WordPress 6.
    Stub env values should be empty strings and not null
    to avoid issues with strpos() in link-template.php.

--- trellis/roles/deploy/files/tmp_multisite_constants.php
+++ trellis/roles/deploy/files/tmp_multisite_constants.php
@@ -2,6 +2,6 @@
 error_reporting(E_ALL & ~E_NOTICE);
 define('MULTISITE', false);
 define('SUBDOMAIN_INSTALL', false);
-define('WPMU_PLUGIN_DIR', null);
-define('WP_PLUGIN_DIR', null);
+define('WPMU_PLUGIN_DIR', '');
+define('WP_PLUGIN_DIR', '');
 define('WP_USE_THEMES', false);
