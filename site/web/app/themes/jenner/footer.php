6d4b1995d3ed9b0405e7b128a814d0bb7316290e
Reid Burke <reid@reidburke.com>
2022-09-16 14:06:15 -0700

    Unified footer.

--- site/web/app/themes/jenner/footer.php
+++ site/web/app/themes/jenner/footer.php
@@ -1,21 +1,5 @@
-            <footer class="footer" role="contentinfo">
-                <div class="footerwhite">
-                    <div class="wrap">
-                        <?php block_template_part( 'Social Icons' ); ?>
-                    </div>
-                </div>
-                <div id="inner-footer" class="wrap clearfix" itemscope itemtype="http://schema.org/NGO">
-                    <div class="widget">
-                        <?php block_template_part( 'Footer' ); ?>
-                    </div>
-                    <p class="source-org copyright">
-                        <span itemprop="name"><?php bloginfo( 'name' ); ?></span> exists to awaken this generation to new life in Jesus Christ.
-                        &bull; &copy; <?php echo date('Y'); ?>
-						&bull; <a href="https://awakeningchurch.com/privacy/">Privacy</a>
-                    </p>
-
-				</div> <?php // end #inner-footer ?>
-
+            <footer>
+                    <?php block_template_part( 'Footer' ); ?>
 			</footer> <?php // end footer ?>
 
 		</div> <?php // end #container ?>
