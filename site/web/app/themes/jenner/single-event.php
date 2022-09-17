efeb3a8a10ab8934d532cac7b6525abaaf6c60a4
Reid Burke <reid@reidburke.com>
2022-09-17 12:07:30 -0700

    Remove article-header from events pages.

--- site/web/app/themes/jenner/single-event.php
+++ site/web/app/themes/jenner/single-event.php
@@ -10,21 +10,6 @@
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
-								<header class="article-header">
-
-									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
-
-                                    <p class="byline">
-                                        <?php $event_start_date = get_post_meta($post->ID, 'event-start-date', true); ?>
-                                        <?php printf('<time class="start" datetime="%1$s">%2$s</time>', date('Y-m-j', $event_start_date), date(get_option('date_format'), $event_start_date)); ?>
-                                        <?php $event_end_date = get_post_meta($post->ID, 'event-end-date', true); ?>
-                                        <?php if ($event_start_date != $event_end_date) : ?>
-                                            &mdash; <?php printf('<time class="end" datetime="%1$s">%2$s</time>', date('Y-m-j', $event_end_date), date(get_option('date_format'), $event_end_date)); ?>
-                                        <?php endif; ?>
-                                    </p>
-
-								</header> <?php // end article header ?>
-
 								<section class="entry-content clearfix" itemprop="articleBody">
 									<?php the_content(); ?>
                                 </section> <?php // end article section ?>
