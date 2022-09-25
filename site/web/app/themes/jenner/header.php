c5c3dbc6e1397978993b06d858ea27bdfd8c5714
Reid Burke <reid@reidburke.com>
2022-09-24 21:13:20 -0700

    Remove shortcut icon.
    This is done by WordPress now based on the site icon.
    The wp_head() takes care of it with rel="icon".
    
    See: https://developer.mozilla.org/en-US/docs/Web/HTML/Link_types
    See: https://stackoverflow.com/a/44124060

--- site/web/app/themes/jenner/header.php
+++ site/web/app/themes/jenner/header.php
@@ -11,9 +11,6 @@
 
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-		<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
-		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-
         <link rel="preconnect" href="https://awakening.azureedge.net">
 
 		<?php wp_head(); ?>
